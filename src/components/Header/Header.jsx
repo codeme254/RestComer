88d0b9ac19c8b8392ef4327fbe9ec5994a8e2e10
otwoma <otwomadennis365@gmail.com>
2021-11-22 10:05:37 +0300

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <div>
+            <h1>this is the header of the pestlibro website</h1>
+        </div>
+    )
+}
+export default Header;
