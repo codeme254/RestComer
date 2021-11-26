0c54320eedd6fbde5e5ad5e2dec2240f77c13575
otwoma <otwomadennis365@gmail.com>
2021-11-26 15:16:57 +0300

    created the all meals section of the website

---
+++ src/components/AllMeals/AllMeals.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import TertiaryHeading from "../Typography/SecondaryHeading";
+import Meal from "./Meal";
+import vegs from '../../Assets/Images/vegetables.png'
+import hotdog from '../../Assets/Images/hotdog.png'
+import plateFood from '../../Assets/Images/plateFood.png'
+import food3 from '../../Assets/Images/food-3.jpg';
+import pizza from '../../Assets/Images/pizza.png';
+import hotdogg2 from '../../Assets/Images/hotdogg2.png';
+import sandwich from '../../Assets/Images/sandwich.png';
+
+const AllMeals = () => {
+    return(
+        <section className="meals">
+            <TertiaryHeading text = "this is what we have for you" />
+            <div className="meals__container">
+                <Meal
+                    discount="15%"
+                    imageLink={hotdog}
+                    mealName="hamburger"
+                    mealDescription="Bearing And Let Day Created Moving
+                    Without Image One Heaven Third Hath
+                    Firmament heaven yielding third years, also bearing moveth good also. Evening upon life darkness whales brought they're, for beginning said sea fifth, whales isn't. I darkness set. Two god seas."
+                 />
+                <Meal
+                    discount="15%"
+                    imageLink={pizza}
+                    mealName="pepperoni pizza"
+                    mealDescription="Bearing And Let Day Created Moving
+                    Without Image One Heaven Third Hath
+                    Firmament heaven yielding third years, also bearing moveth good also. Evening upon life darkness whales brought they're, for beginning said sea fifth, whales isn't. I darkness set. Two god seas."
+                 />
+                <Meal
+                    discount="15%"
+                    imageLink={sandwich}
+                    mealName="sandwich"
+                    mealDescription="Bearing And Let Day Created Moving
+                    Without Image One Heaven Third Hath
+                    Firmament heaven yielding third years, also bearing moveth good also. Evening upon life darkness whales brought they're, for beginning said sea fifth, whales isn't. I darkness set. Two god seas."
+                 />
+                <Meal
+                    discount="15%"
+                    imageLink={hotdogg2}
+                    mealName="hotdogs"
+                    mealDescription="Bearing And Let Day Created Moving
+                    Without Image One Heaven Third Hath
+                    Firmament heaven yielding third years, also bearing moveth good also. Evening upon life darkness whales brought they're, for beginning said sea fifth, whales isn't. I darkness set. Two god seas."
+                 />
+            </div>
+            <div className="all-meals__button">
+                <a href="/" className="all-meals__button--btn">discover all our meals</a>
+            </div>
+        </section>
+    )
+};
+export default AllMeals;
